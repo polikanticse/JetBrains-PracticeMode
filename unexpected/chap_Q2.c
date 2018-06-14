c6faf303670c0b3b570d69c583b8a0a447659c49
polikanticse <polikanticse@gmail.com>
2018-06-14 18:45:30 +0530

    chapter 2 question 2

---
+++ unexpected/chap_Q2.c
@@ -0,0 +1,14 @@
+/*
+To declare a variable and print its value
+note : C gives the original value and &c will give you the address of the variable
+ */
+
+
+ #include<stdio.h>
+ void main()
+ {
+     int c;
+     c=10;
+     printf("The value for the variable C : %d",c);
+ }
+
